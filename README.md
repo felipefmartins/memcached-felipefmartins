5ffc08cfc4b4d533604ee1af03b22f677aa52aa0
root <root@chef-workstation.dexter.com.br>
2018-06-11 00:47:51 -0300

    Instalacao e Configuracao do servidor memcached em debian 9 e centos 7

---
+++ README.md
@@ -0,0 +1,4 @@
+# memcached-felipefmartins
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,3 @@
+include_recipe 'memcached-felipefmartins::install'
+include_recipe 'memcached-felipefmartins::configure'
+include_recipe 'memcached-felipefmartins::service'
